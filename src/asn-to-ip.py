58237662910e58ff65d3d599eb50fab5ac6aeec3
Doug Dimick <doug@dimick.net>
2020-01-12 12:31:28 -0800

    now using radb query flags instead of ripe, easier parsing of results; code cleanup

--- src/asn-to-ip.py
+++ src/asn-to-ip.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-import re
-import socket
 import argparse
 import ipaddress
+import re
+import socket
 
+## Get command-line options.
 _parser = argparse.ArgumentParser()
 _parser_group = _parser.add_mutually_exclusive_group(required = True)
 _parser_group.add_argument('-a', '--asn', action = 'append', help = 'BGP Autonomous System Number (ASN), may use this parameter multiple times')
@@ -17,11 +18,10 @@ _parser_args = _parser.parse_args()
 
 
 ## Controls socket communication to whois server.
-def whois(_domain, _server = 'whois.radb.net', _port = 43):
+def whois(_query, _server = 'whois.radb.net', _port = 43, _result = str()):
   _sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   _sock.connect((_server, _port))
-  _sock.send(_domain.encode('utf-8'))
-  _result = str()
+  _sock.send(_query.encode('utf-8'))
 
   while True:
     _data = _sock.recv(1024).decode('iso-8859-1')
@@ -34,41 +34,37 @@ def whois(_domain, _server = 'whois.radb.net', _port = 43):
   return(_result)
 
 
-## Performs the whois query and returns a multiline string of the resulting IPv4 and/or IPv6 networks.
-def get_asn_networks(_asn, _ipv4 = True, _ipv6 = False):
-  _route_type = str()
-  if _ipv6:
-    _route_type = '[6]?' if _ipv4 else '6'
+## Gets a list of networks from the list of ASNs. Returns a sorted and deduplicated multi-line string.
+def get_network_list(_result = list()):
 
-  _result = whois('-i origin {0}\r\n'.format(_asn))
+  for _asn in _parser_args.asn:
+    # Validate we have a valid-looking ASN.
+    _valid_asn = re.search('(?:AS)?(\d{1,10})', _asn, re.IGNORECASE)
+    if _valid_asn:
+      _valid_asn = 'AS{0}'.format(_valid_asn.group(1))
+    else:
+      return('Invalid ASN "{0}". Example valid format is "AS54321" or simply "54321".'.format(_asn))
 
-  if _result:
-    _networks = re.findall('^route{0}:\s+(.*?)$'.format(_route_type), _result, re.MULTILINE)
+    # Perform the whois query for IPv4 networks.
+    _asn_networks = list(whois('!g{0}\n'.format(_valid_asn)).split())
 
-    if _networks:
-      return(_networks)
+    # Perform the whois query for IPv6 networks (if requested).
+    if _parser_args.ipv6:
+      _asn_networks += list(whois('!6{0}\n'.format(_valid_asn)).split())
 
-  return(None)
+    # Builds an object containing valid networks, removing duplicates.
+    for _network in set(_asn_networks):
+      try:
+        _result.append(ipaddress.ip_network(_network, strict = False))
+      except ValueError:
+        # Ignore anything that isn't a valid network.
+        continue
 
-
-## Deduplicates and sorts a list of networks from the list of ASNs. Returns a multi-line string.
-def get_network_list():
-  _networks = list()
-
-  for _asn in range(0, len(_parser_args.asn)):
-    _valid_asn = re.search('(?:AS)?(\d{1,10})', _parser_args.asn[_asn], re.IGNORECASE)
-    if not _valid_asn:
-      return('Invalid ASN "{0}". Example valid format is "AS54321" or simply "54321".'.format(_parser_args.asn[_asn]))
-    _valid_asn = 'AS{0}'.format(_valid_asn.group(1))
-
-    # Builds an object containing networks, removing duplicates.
-    for _network in list(set(get_asn_networks(_valid_asn, True, _parser_args.ipv6))):
-      _networks.append(ipaddress.ip_network(_network, strict = False))
-
-  return('\n'.join(map(str, sorted(_networks, key=ipaddress.get_mixed_type_key))))
+  return('\n'.join(map(str, sorted(_result, key=ipaddress.get_mixed_type_key))))
 
 
 if __name__  ==  '__main__':
+
   ## Command-line mode
   if _parser_args.asn:
     print(get_network_list())
@@ -82,7 +78,6 @@ if __name__  ==  '__main__':
     @app.route('/')
     def index():
       if request.args.get('asn') is not None:
-        # We aren't really using argparse here but, since the variable already exists, it's simple to reuse.
         _parser_args.asn = request.args.get('asn').split(',')
         _parser_args.ipv6 = True if request.args.get('ipv6') is not None else False
         return(get_network_list())
@@ -90,3 +85,4 @@ if __name__  ==  '__main__':
       return('usage: {0}?asn=X, where X is one or more BGP Autonomous System Numbers separated by commas.\nReturns IPv4 results by default. Add &ipv6 to also include IPv6 results, like {0}?asn=X&ipv6.'.format(request.url_root))
 
     app.run(debug = _parser_args.debug, host = _parser_args.ip, port = _parser_args.port)
+
